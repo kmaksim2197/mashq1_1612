482acf0895bec3c16ca3893621e8571935fa154c
kmaksim2197 <kmaksim2197@gmail.com>
2025-12-16 15:39:38 +0500

    Create main.sql

---
+++ main.sql
@@ -0,0 +1,23 @@
+CREATE TABLE Talabalar (
+    yosh INTEGER,
+    ism TEXT,
+    GPA REAL,
+    telefon TEXT NULL
+);
+
+CREATE TABLE Mahsulotlar (
+    narx REAL,
+    tavsif TEXT
+);
+
+CREATE TABLE Xodimlar (
+    oylik REAL NULL
+);
+
+CREATE TABLE Kitoblar (
+    sahifalar_soni INTEGER
+);
+
+CREATE TABLE Foydalanuvchilar (
+    email TEXT NULL
+);
